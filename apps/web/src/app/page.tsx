9cac2b29a411edc2b57985b6ad252efd509a976b
Brahim <brahimhalas@gmail.com>
2025-08-13 11:47:18 +0200

    Refactor Home component layout with Tailwind CSS for improved styling and responsiveness

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -12,27 +12,20 @@ async function getHealth() {
 export default async function Home() {
   const health = await getHealth();
   return (
-    <main style={{ padding: 24 }}>
-      <h1>Spring Boot + Next.js</h1>
-      <p>
-        API health: <strong>{String(health.ok)}</strong>
-      </p>
-      <p>
-        Env <code>NEXT_PUBLIC_API_URL</code>:{" "}
-        {process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080"}
-      </p>
-      <p style={{ marginTop: 16 }}>
-        <a href="/todos" style={{ textDecoration: "underline" }}>
-          ➡️ Aller à la liste des todos
-        </a>
-      </p>
-
-      <div className="p-4 rounded-lg border border-orange/40 bg-sand/20 text-ink">
-  Bloc palette OK
-</div>
-<button className="mt-3 px-4 py-2 rounded-lg bg-teal hover:bg-teal/90 text-white">
-  Bouton teal
-</button>
+    <main style={{ padding: 24 }} className="min-h-[100svh] grid place-items-center bg-teal p-6">
+      <div className="bg-transparent text-white p-4 rounded-none shadow-none ring-0 outline-none">
+        <h1>Spring Boot banckend with Next.js frontend</h1>
+        <p> API health: <strong>{String(health.ok)}</strong> </p>
+        <p>
+          Env <code>NEXT_PUBLIC_API_URL</code>:{" "}
+          {process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080"}
+        </p>
+        <p style={{ marginTop: 16 }}>
+          <a href="/todos" style={{ textDecoration: "underline" }}>
+            ➡️ Aller à la liste des TODOs
+          </a>
+        </p>
+      </div>
     </main>
   );
 }
